7cd223caeab0fdebd83aa3b559587f15601c6c2f
Gwennael Jean <gwennael.jean@trsb.net>
2017-03-07 18:31:01 +0100

    chore: revision sur le skeleton

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src\Controller;
+
+use Tourbillon\Controller\Controller;
+
+/**
+ * Description of IndexController
+ *
+ * @author gjean
+ */
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
